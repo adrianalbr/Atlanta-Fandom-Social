b6505d49c7f4b7dd70863bafbb5060a1a27973b8
Dani JM <xdanielmtz@gmail.com>
2021-01-30 13:44:34 -0500

    added server.js and installed mongoose and express

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("client/build"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/mern-starter",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  }
+);
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected!");
+});
+
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: ", err);
+});
+
+app.get("/api/config", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
