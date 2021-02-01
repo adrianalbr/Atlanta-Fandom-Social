7c1dd3d0a16888d713416b8ded8b997e17242bda
Arpita Saha <arpita.sahaa@gmail.com>
2021-02-01 13:55:21 -0500

    MERN starter code

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 
 const mongoose = require("mongoose");
 // const routes = require("./routes");
@@ -12,16 +13,19 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
-// app.use(routes);
-app.get("/" , (req,res)=>{
-    res.send("Hello world");
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fandomSocial");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/fandomSocial",
+  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+);
 
 // Start the API server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
