dfbd64e326f6a80e835ff1a34e9ff1968a2b0265
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-02-02 18:15:53 -0500

    pushing latest of sign in page to fix some CSS with the team

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
-import Home from './Containers/Home/Home';
-import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./Containers/Home/Home";
+import Navbar from "./Components/Navbar/Navbar";
+import Login from "./Containers/Login/Login";
 
 function App() {
   return (
     <div className="App">
       <Router>
-       <Navbar/>
-       <Switch>
-         <Route path="/" component={Home}/>
-       </Switch>
-       {/* <Footer/> */}
-     </Router>
+        <Navbar />
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/" component={Home} />
+        </Switch>
+        {/* <Footer/> */}
+      </Router>
     </div>
   );
 }
