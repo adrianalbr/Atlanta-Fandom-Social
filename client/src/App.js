22b726a2c960b0f6bfffb40defdf3d0665a317ea
Adriana Bonilla-Ramirez <adrianalbr12@gmail.com>
2021-02-01 20:48:23 -0500

    deleted unnecessary files, created home, navbar, and cardpost

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
 import Home from './Containers/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
-       {/* <Navbar/> */}
+       <Navbar/>
        <Switch>
          <Route path="/" component={Home}/>
        </Switch>
