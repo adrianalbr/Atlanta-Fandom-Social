c961ec055e64dea2085e5a44aad7defa344b231c
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-02-03 18:41:06 -0500

    added signup and css css for both signup and login are WIP

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Containers/Home/Home";
 import Navbar from "./Components/Navbar/Navbar";
 import Login from "./Containers/Login/Login";
+import Signup from "./Containers/Signup/Signup";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
+          <Route path="/signup" component={Signup} />
           <Route path="/login" component={Login} />
           <Route path="/" component={Home} />
         </Switch>
