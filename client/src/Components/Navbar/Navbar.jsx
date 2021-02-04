9686ff6f2a77063ce59c56c751f8539bba82f036
Adriana Bonilla-Ramirez <adrianalbr12@gmail.com>
2021-02-03 20:31:09 -0500

    navbar, menu and card work

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Navbar.css";
+import { InputGroup, Button, InputGroupAddon, Input } from "reactstrap";
+import Cloak from "../../assets-sort/icons/cloak.png";
 
 const Navbar = () => {
   return (
@@ -7,39 +9,40 @@ const Navbar = () => {
       <nav>
         <div className="nav-wrapper">
           <a href="/" class="brand-logo"></a>
-          <ul id="nav-mobile" className="center hide-on-med-and-down">
-          <li>
-              <img src="./assests-sort/icons/cloak.png" alt="cloak"/>
-              <a href="badges.html">Posts</a>
-            </li>
-            
-            <li>
-              <div class="container">
-              <form id="search-site"  action='search' method='get'>
-                <div class="input-group">
-                  <div class="input-field">
-                  <input id="search" type="search" name='q'/>
-                  <label class="label-icon" for="search">
-                    <i class="material-icons" >search</i>
-                  </label>
-                  </div>
-                  <button type="submit" class="input-group-addon btn">search</button>
-                </div>
-              </form>
-            </div>
-            </li>
-            <li>
-              <a href="badges.html">Posts</a>
-            </li>
-            <li>
-              <a href="badges.html">Profile</a>
-            </li>
-            <li>
-              <a href="collapsible.html">Other little buttons</a>
-            </li>
-          </ul>
+          {/* <ul id="nav-mobile" className="center hide-on-med-and-down"> */}
+          {/* <li> */}
+          <img src={ Cloak } alt="cloak" />
+          <a href="/">Home</a>
+          {/* </li> */}
+
+          {/* <li> */}
+          <div class="container">
+            <form id="search-site" action="search" method="get">
+              <InputGroup>
+                <Input className="input-nav" style={{ width: "300px" }} />
+                <InputGroupAddon addonType="append">
+                  <Button color="secondary" style={{ marginTop: "10px" }}>
+                    Search!
+                  </Button>
+                </InputGroupAddon>
+              </InputGroup>
+            </form>
+          </div>
+          {/* </li> */}
+          <div>
+            <ul>
+              <li>
+                <a href="badges.html">Posts</a>
+              </li>
+              <li>
+                <a href="badges.html">Profile</a>
+              </li>
+              <li>
+                <a href="collapsible.html">Other little buttons</a>
+              </li>
+            </ul>
+          </div>
         </div>
-         
       </nav>
     </div>
   );
