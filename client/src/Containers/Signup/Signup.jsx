ef10a9d147204d9c329aa0408dffbfe2a18cf019
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-02-03 20:35:36 -0500

    added welcome containers

--- client/src/Containers/Signup/Signup.jsx
+++ client/src/Containers/Signup/Signup.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import "./Signup.css";
+// import "./Signup.css";
 
 const Signup = () => {
     return (
--- client/src/Containers/WelcomeTopics/WelcomeTopics.jsx
+++ client/src/Containers/WelcomeTopics/WelcomeTopics.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const WelcomeTopics = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+return (
+    <div>
+    <div className="titleHeaderContainer">
+        <h2 className="Header">Welcome to ATL Fandom Social</h2>
+        <h5 className="Subtitle">Please Choose Some Topics</h5>
+        <div className="ContentContainer"></div>
+    </div>
+    <div class="rightSide"></div>
+    </div>
+);
 };
 
 export default WelcomeTopics;
