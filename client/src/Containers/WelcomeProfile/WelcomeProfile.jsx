f68484be296fcb26ca90341f33341059e1d9b142
Shelby De Oliveira Cignetti <shelbycignetti@gmail.com>
2021-02-03 21:10:15 -0500

    added blank css for the welcome

--- client/src/Containers/WelcomeProfile/WelcomeProfile.jsx
+++ client/src/Containers/WelcomeProfile/WelcomeProfile.jsx
@@ -1,15 +1,14 @@
 import React from "react";
+// import "./WelcomeProfile.css";
 
 const WelcomeProfile = () => {
 return (
     <div>
-    <div className="titleHeaderContainer">
+    <div className="MainContainer">
         <h2 className="Header">Welcome to ATL Fandom Social</h2>
             <h5 className="Subtitle">Please Choose a Profile Picture</h5>
             <div className="ContentContainer">
             </div>
-    </div>
-    <div class="rightSide">
     </div>
     </div>
 );
