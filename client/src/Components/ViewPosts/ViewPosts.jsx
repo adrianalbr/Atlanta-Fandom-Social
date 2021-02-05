4e3e367ba8e31400d1fe453bfaeae3f4e9927c65
Daniel Martinez <xdanielmtz@gmail.com>
2021-02-04 23:00:54 -0500

    brought in 2 hooks and axios as well. Linked the component inside the home parent component

---
+++ client/src/Components/ViewPosts/ViewPosts.jsx
@@ -0,0 +1,13 @@
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const ViewPosts = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ViewPosts;
