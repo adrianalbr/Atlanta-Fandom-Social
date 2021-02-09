81ec028404d76bb94cde230484728661d42c5f90
Arpita Saha <arpita.sahaa@gmail.com>
2021-02-09 12:23:52 -0500

    display author on screen

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -1,25 +1,24 @@
 const db = require("../models/");
 
 module.exports = {
-
   findByAuthor: function (req, res) {
-  console.log("Find by author");
-    db.Content.find({author : req.user._id})
+    console.log("Find by author");
+    db.Content.find({ author: req.user._id })
       .sort({ date: -1 })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
 
-
   findAll: function (req, res) {
     db.Content.find(req.query)
+      .populate("author")
       .sort({ date: -1 })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
 
   findById: function (req, res) {
-    console.log(req.params.id)
+    console.log(req.params.id);
     db.Content.findById(req.params.id)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
@@ -27,7 +26,7 @@ module.exports = {
 
   create: function (req, res) {
     console.log("in conrnt controller create all");
-    console.log(req.body)
+    console.log(req.body);
     const content = req.body;
     content.author = req.user._id;
     db.Content.create(content)
