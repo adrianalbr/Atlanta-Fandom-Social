cdf785cf59ab770dd935db2bf1a6b9833273d8b4
Adriana Bonilla-Ramirez <adrianalbr12@gmail.com>
2021-02-08 21:06:33 -0500

    adding buttons and style to myposts

--- client/src/Containers/Profile/Profile.jsx
+++ client/src/Containers/Profile/Profile.jsx
@@ -2,43 +2,58 @@ import { React, useState, useEffect } from "react";
 import axios from "axios";
 import "./Profile.css";
 import Navbar from "../../Components/Navbar/Navbar";
+import Menu from "../../Components/Menu/Menu";
 
 const Profile = (props) => {
-const [posts, setPosts] = useState([]);
-useEffect(() => {
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
     // api call to get all posts of that user
     axios
-    .get("/api/content/user", {
+      .get("/api/content/user", {
         headers: {
-        Authorization: props.token,
+          Authorization: props.token,
         },
-    })
-    .then((response) => {
+      })
+      .then((response) => {
         console.log(response.data);
         setPosts(response.data);
-    });
-}, []);
+      });
+  }, []);
 
-return (
+  return (
     <div>
-    <Navbar />
-    {posts.map(({ category, title, date, contentText }, index) => (
-        <div className="row" key={index}>
-        <div className="card">
-            <div className="card-content white-text">
-            <span className="card-title">{title}</span>
-            <p>{contentText}</p>
-            </div>
-            <div className="card-action">
-            <p>
-                Category: {category}, Date: {date}
-            </p>
-            </div>
+      <div className="row">
+        <Navbar />
+        <div className="col s4">
+          <Menu />
         </div>
-        </div>
-    ))}
+
+        {posts.map(({ category, title, date, contentText }, index) => (
+          <div className="col s5">
+            <div className="row" key={index}>
+              <div className="card">
+                <div className="card-content white-text">
+                  <span className="card-title">{title}</span>
+                  <p>{contentText}</p>
+                </div>
+                <div className="card-action">
+                  <button>
+                    <i className="fa fa-trash-o"></i> Delete
+                  </button>
+                  <button>
+                    <i className="fa fa-pencil"></i> Update
+                  </button>
+                  <p>
+                    Category: {category}, Date: {date}
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-);
+  );
 };
 
 export default Profile;
