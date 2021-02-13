1b1b4edca272e2c548e99916735f001df1cc5ce6
Adriana Bonilla-Ramirez <adrianalbr12@gmail.com>
2021-02-13 13:06:21 -0500

    changes profile to my posts and added style to post

--- client/src/Containers/Profile/Profile.jsx
+++ client/src/Containers/Profile/Profile.jsx
@@ -59,10 +59,10 @@ const Profile = (props) => {
               <div className="card">
                 <div className="card-content white-text">
                   <span className="card-title">{title}</span>
-                  <img className="cardImage" src={imageURL}/>
+                  <img className="cardImage" src={imageURL} alt="Image from Card"/>
                   <a href={url}>URL</a>
-                  <img src={imageURL}/>
-                  <a href={url}>{url}</a>
+                  {/* <img src={imageURL}/>
+                  <a href={url}>{url}</a> */}
                   <p>{contentText}</p>
 
                 </div>
