3e368748d423f2fa6786ec5d80e383684d41dd23
Dani JM <xdanielmtz@gmail.com>
2021-02-13 10:33:29 -0500

    fixed the issues

--- client/src/Components/ViewPosts/ViewPosts.jsx
+++ client/src/Components/ViewPosts/ViewPosts.jsx
@@ -28,7 +28,7 @@ const ViewPosts = (props) => {
                   <h6>
                     Author: {author.firstName + " " + author.lastName},
                     Category: {category}, Date: {date.substring(0, 10)}
-                    <a href={url}>{url}</a>
+                    <a href={url}>{url} </a>
                   </h6>
                   <div />
                 </div>
