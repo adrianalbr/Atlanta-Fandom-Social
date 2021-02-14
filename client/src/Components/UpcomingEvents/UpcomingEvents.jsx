539bc96ab154b7c03b3abc619f2a52c96a02af15
Arpita Saha <arpita.sahaa@gmail.com>
2021-02-13 20:00:55 -0500

    added upcoming event list

---
+++ client/src/Components/UpcomingEvents/UpcomingEvents.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./UpcomingEvents.css";
+
+function UpcomingEvents() {
+  return (
+    <div className="upcomingEventsBox">
+        <div>
+        <table>
+        <tr>
+          <th>Upcoming Events</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+        </tr>
+        <tr>
+          <td>Ernst Handel</td>
+        </tr>
+        <tr>
+          <td>Island Trading</td>
+        </tr>
+        <tr>
+          <td>Laughing Bacchus Winecellars</td>
+        </tr>
+        <tr>
+          <td>Magazzini Alimentari Riuniti</td>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+        </tr>
+        <tr>
+          <td>Ernst Handel</td>
+        </tr>
+        <tr>
+          <td>Island Trading</td>
+        </tr>
+        <tr>
+          <td>Laughing Bacchus Winecellars</td>
+        </tr>
+        <tr>
+          <td>Magazzini Alimentari Riuniti</td>
+        </tr>
+      </table>
+        </div>
+      
+    </div>
+  );
+}
+
+export default UpcomingEvents;
