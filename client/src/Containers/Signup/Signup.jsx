078fae4393403413d986a93132adf771fc0b663e
Daniel Martinez <xdanielmtz@gmail.com>
2021-02-14 21:51:44 -0500

    fixed background image in the signup page not showing up at the bottom

--- client/src/Containers/Signup/Signup.jsx
+++ client/src/Containers/Signup/Signup.jsx
@@ -45,105 +45,107 @@ const Signup = (props) => {
     return <Redirect to={redirect} />;
   }
   return (
-    <div className="main">
-      {errors.map((error) => (
-        <Alert color="danger">{error.message}!</Alert>
-      ))}
+    <>
+        {errors.map((error) => (
+          <Alert color="danger">{error.message}!</Alert>
+        ))}
 
-      <div>
-        {/* <h2 className="formHeader">Welcome to ATL Fandom Social</h2> */}
-        <div className="container center-align">
-          <div className="row">
-            <div className="col s12">
-              <img
-                className="formHeader"
-                src={Logo}
-                alt="Where Fans Come Together"
-              ></img>
+        <div>
+          {/* <h2 className="formHeader">Welcome to ATL Fandom Social</h2> */}
+          <div className="container center-align">
+            <div className="row">
+              <div className="col s12">
+                <img
+                  className="formHeader"
+                  src={Logo}
+                  alt="Where Fans Come Together"
+                ></img>
+              </div>
+            </div>
+          </div>
+
+          <div className="container center-align formContainer">
+            <div className="row rowTwo">
+              <div className="col s6">
+                <form>
+                  {/* <label htmlFor="firstName">First Name</label> */}
+                  <input
+                    type="text"
+                    name="firstName"
+                    onChange={handleChange}
+                    value={userSignUp.firstName}
+                    className="formInput"
+                    placeholder="First Name"
+                    required
+                  />
+                  {/* <label htmlFor="lastName">Last Name</label> */}
+                  <input
+                    type="text"
+                    name="lastName"
+                    onChange={handleChange}
+                    value={userSignUp.lastName}
+                    className="formInput"
+                    placeholder="Last Name"
+                    required
+                  />
+                  {/* <label htmlFor="Username">Username</label> */}
+                  <input
+                    type="text"
+                    name="username"
+                    onChange={handleChange}
+                    value={userSignUp.username}
+                    className="formInput"
+                    placeholder=" Username"
+                    required
+                  />
+                  {/* <label htmlFor="email">Email</label> */}
+                  <input
+                    type="text"
+                    name="email"
+                    onChange={handleChange}
+                    value={userSignUp.email}
+                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+                    className="formInput"
+                    placeholder="Email"
+                    required
+                  />
+                  {/* <label htmlFor="password">Password</label> */}
+                  <input
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    value={userSignUp.password}
+                    className="formInput"
+                    placeholder="Password"
+                    required
+                  />
+                  <input
+                    id="signUp"
+                    type="submit"
+                    className="submitButton"
+                    onClick={handleSubmit}
+                    name="submitButton"
+                    value="SignUp"
+                  />
+                </form>
+              </div>
+              <div className="col s6 center-align">
+                <h5 className="goToSignUp">
+                  <img id="ourLogo" src={ourLogo} />
+                  <br />
+                  <br />
+                  Already have an account? <br />
+                </h5>
+                <button id="loginButton" className="submitButton">
+                  <a id="loginAtag" href="/login">
+                    Login Here
+                  </a>
+                </button>
+              </div>
             </div>
           </div>
         </div>
-
-        <div className="container center-align formContainer">
-          <div className="row rowTwo">
-            <div className="col s6">
-              <form>
-                {/* <label htmlFor="firstName">First Name</label> */}
-                <input
-                  type="text"
-                  name="firstName"
-                  onChange={handleChange}
-                  value={userSignUp.firstName}
-                  className="formInput"
-                  placeholder="First Name"
-                  required
-                />
-                {/* <label htmlFor="lastName">Last Name</label> */}
-                <input
-                  type="text"
-                  name="lastName"
-                  onChange={handleChange}
-                  value={userSignUp.lastName}
-                  className="formInput"
-                  placeholder="Last Name"
-                  required
-                />
-                {/* <label htmlFor="Username">Username</label> */}
-                <input
-                  type="text"
-                  name="username"
-                  onChange={handleChange}
-                  value={userSignUp.username}
-                  className="formInput"
-                  placeholder=" Username"
-                  required
-                />
-                {/* <label htmlFor="email">Email</label> */}
-                <input
-                  type="text"
-                  name="email"
-                  onChange={handleChange}
-                  value={userSignUp.email}
-                  pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
-                  className="formInput"
-                  placeholder="Email"
-                  required
-                />
-                {/* <label htmlFor="password">Password</label> */}
-                <input
-                  type="password"
-                  name="password"
-                  onChange={handleChange}
-                  value={userSignUp.password}
-                  className="formInput"
-                  placeholder="Password"
-                  required
-                />
-                <input
-                  id="signUp"
-                  type="submit"
-                  className="submitButton"
-                  onClick={handleSubmit}
-                  name="submitButton"
-                  value="SignUp"
-                />
-              </form>
-            </div>
-            <div className="col s6 center-align">
-              <h5 className="goToSignUp">
-                <img id="ourLogo" src={ourLogo} />
-                <br />
-                <br />
-                Already have an account? <br />
-              </h5>
-              <button id="loginButton" className="submitButton">
-                <a id="loginAtag" href="/login">Login Here</a>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
